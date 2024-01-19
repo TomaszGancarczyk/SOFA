c19716df4fcf9b5089b4934901b6317184f1fbbe
Tomasz Gancarczyk <tom.g@onet.pl>
2024-01-19 12:01:27 +0100

    Added view for timers

---
+++ Guide/Controllers/TimerController.cs
@@ -0,0 +1,32 @@
+﻿using Error.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Timer.Controllers
+{
+    public class TimerController : Controller
+    {
+        private readonly ILogger<TimerController> _logger;
+
+        public TimerController(ILogger<TimerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
