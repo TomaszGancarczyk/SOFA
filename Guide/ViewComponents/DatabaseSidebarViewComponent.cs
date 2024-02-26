20874bf6587e250965a828059a4c788fb009ab88
Tomasz Gancarczyk <tom.g@onet.pl>
2024-02-26 10:54:47 +0100

    Added buttons to go back to sidebar menu and fixed invalid scrolling

--- Guide/ViewComponents/DatabaseSidebarViewComponent.cs
+++ Guide/ViewComponents/DatabaseSidebarViewComponent.cs
@@ -8,26 +8,15 @@ using System.Net.Mail;
 
 namespace Guide.ViewComponents
 {
-    public class DatabaseSidebar : ViewComponent
+    public class DatabaseSidebar(ILogger<DatabaseController> logger, List<ArtefactModel> artefacts, List<ContainerModel> containers, List<ArmorModel> armor, List<WeaponModel> weapons, List<AttachmentModel> attachments, List<BulletModel> bullets) : ViewComponent
     {
-        private readonly ILogger<DatabaseController> _logger;
-        private readonly List<ArtefactModel> _artefacts;
-        private readonly List<ContainerModel> _containers;
-        private readonly List<ArmorModel> _armors;
-        private readonly List<WeaponModel> _weapons;
-        private readonly List<AttachmentModel> _attachments;
-        private readonly List<BulletModel> _bullets;
-
-        public DatabaseSidebar(ILogger<DatabaseController> logger, List<ArtefactModel> artefacts, List<ContainerModel> containers, List<ArmorModel> armor, List<WeaponModel> weapons, List<AttachmentModel> attachments, List<BulletModel> bullets)
-        {
-            _logger = logger;
-            _artefacts = artefacts;
-            _containers = containers;
-            _armors = armor;
-            _weapons = weapons;
-            _attachments = attachments;
-            _bullets = bullets;
-        }
+        private readonly ILogger<DatabaseController> _logger = logger;
+        private readonly List<ArtefactModel> _artefacts = artefacts;
+        private readonly List<ContainerModel> _containers = containers;
+        private readonly List<ArmorModel> _armors = armor;
+        private readonly List<WeaponModel> _weapons = weapons;
+        private readonly List<AttachmentModel> _attachments = attachments;
+        private readonly List<BulletModel> _bullets = bullets;
 
         public IViewComponentResult Invoke()
         {
